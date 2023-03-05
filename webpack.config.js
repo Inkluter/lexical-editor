b669ea564904d1392ef162476bbeed16d8c2ce50
Ivan Leshchenko <ivan.leshchenko@rxdefine.com>
2023-03-06 01:02:38 +0200

    lint script added, files linted, .eslintrc updated

--- webpack.config.js
+++ webpack.config.js
@@ -2,32 +2,32 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: "./src/index.tsx",
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "build"),
+  entry: "./src/index.tsx",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "build"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "build"),
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, "public", "index.html"),
-        }),
+    port: 8000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
     ],
-    devServer: {
-        static: {
-            directory: path.join(__dirname, "build"),
-        },
-        port: 8000,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(ts|tsx)$/,
-                exclude: /node_modules/,
-                use: ["babel-loader"],
-            },
-        ],
-    },
-    resolve: {
-        extensions: ["*", ".js", ".jsx", ".ts", ".tsx"],
-    },
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx", ".ts", ".tsx"],
+  },
 };
